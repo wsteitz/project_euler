c6c8164551eb2b3ec5986ed9215c0caeb002416b
Wolfgang Steitz <wsteitz@gmail.com>
2014-08-18 20:01:59 +0200

    125 + 24

---
+++ 125.scala
@@ -0,0 +1,21 @@
+
+
+
+def isPalindrom(s: String): Boolean = s == s.reverse
+val n = 100000000
+
+
+def foo(current: Int, acc:BigInt, len:Int): List[BigInt] = {
+    if (acc > n) Nil
+    else if (isPalindrom(acc.toString) && len > 1) {
+        acc :: foo(current + 1, acc + current * current, len + 1) }
+        else foo(current + 1, acc + current * current,  len + 1)
+}
+
+val res = (1 to math.sqrt(n).toInt).flatMap(n => foo(n, 0, 0)).toList.distinct
+
+
+println(res.size)
+println(res.sum)
+
+
---
+++ 24.scala
@@ -0,0 +1,11 @@
+
+
+
+
+val res = List(0,1,2,3, 4, 5, 6 ,7,  8, 9).permutations.take(1000000).mkString
+
+
+println(res.size)
+
+
+println(res)
