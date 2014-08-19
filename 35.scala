5706baf49b82621e709a14cea775090c79740cca
Wolfgang Steitz <wsteitz@gmail.com>
2014-08-19 19:32:29 +0200

    35 & 37

---
+++ 35.scala
@@ -0,0 +1,20 @@
+def isPrime(i: Int): Boolean =
+  if (i <= 1) false
+  else if (i <= 3) true
+  else (2 until math.sqrt(i).toInt + 1).forall(i % _ != 0)
+
+def circle(i: Int) =
+  (0 to i.toString.size - 1).map(multiplier => {
+    val help = math.pow(10, multiplier).toInt
+    ((i % help).toString + (i / help).toString).toInt})
+
+
+def isCircularPrime(i: Int): Boolean = {
+  circle(i).forall(isPrime(_))
+}
+
+val n = 1000000
+
+val res = (2 to n).filter(isCircularPrime(_))
+println(res.size)
+
