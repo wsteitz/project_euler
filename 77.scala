3273b7a7337692c77b110cba9a31036eeccdf8dd
Wolfgang Steitz <wsteitz@gmail.com>
2014-08-31 19:47:47 +0200

    77

---
+++ 77.scala
@@ -0,0 +1,25 @@
+
+package euler
+
+import Primes._
+import Memoize._
+
+
+object Euler077 extends Euler {
+
+    val primes = (1 to 100000).filter(isPrime)
+
+    lazy val count: (Int, Int) => Int = Memoize.memoize{
+        (n, last) => {
+            if (n == 0) 1
+            else if (n == 1 || n < 0) 0
+            else (for {
+                p <- primes.takeWhile(_ <= last)
+            } yield count(n - p, p)).sum
+        }
+    }
+
+    val result = (1 to 10000).find(n => count(n, n) > 5000)
+
+}
+
